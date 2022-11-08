e0235b25c3fd725ea139040b6a58c338868c01ef
COitpark <minhobag18@gmail.com>
2022-11-08 18:16:46 +0900

    11-08

---
+++ data_structure/unit_3_print_string.py
@@ -0,0 +1,15 @@
+# problem
+def Worlds():
+    print("Hello World!")
+    print("Python Programing")
+
+
+
+# problem2
+def World():
+    print("Hello World!")
+    print("Hello World!")
+
+if __name__ == '__main__':
+    Worlds()
+    World()
---
+++ data_structure/unit_5_skill.py
@@ -0,0 +1,6 @@
+# problem
+print(int(0.2467 * 12 + 4.159))
+
+# problem2
+
+
