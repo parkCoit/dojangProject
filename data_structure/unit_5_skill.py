0ab3ea8b018e5c765a664f2957cfad1540a4f4c9
COitpark <minhobag18@gmail.com>
2022-11-10 18:36:20 +0900

    11-10

--- data_structure/unit_5_skill.py
+++ data_structure/unit_5_skill.py
@@ -1,6 +1,14 @@
 # problem
-print(int(0.2467 * 12 + 4.159))
+def num():
+    a = int(input("도로와의 거리(m)")) # 거리 12m
+    print(int(0.2467 * a + 4.159))
 
 # problem2
+def num2():
+    a = int(input("ap 값 : ")) # ap : 102
+    print(float(a*0.6+225))
 
+if __name__ == '__main__':
+    num()
+    num2()
 
---
+++ data_structure/unit_6_everage.py
@@ -0,0 +1,19 @@
+# problem
+def num():
+    a,b,c = map(int, input("숫자 3개 입력 : ").split())
+    print(a+b+c)
+
+# problem2
+def num2():
+    a, b = map(int, input("숫자 2개 입력 :").split())
+    c = input("스트링 값 : ")
+    print(a)
+    print(b)
+    print(c)
+
+def num3():
+    pass
+
+if __name__ == '__main__':
+    num()
+    num2()
